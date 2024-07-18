7fb7bc77d809a0f66e6fa96eb0fefef3014dc9be
yan <yan428@mail.ru>
2024-07-18 22:10:58 +0300

    add prisma generate command 2.0

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM node:20-alpine3.18
 
 WORKDIR /app
 COPY package*.json ./
+
+COPY prisma/schema.prisma prisma/
 RUN npx prisma generate
 
 RUN npm install
--- commit.sh
+++ commit.sh
@@ -3,5 +3,5 @@
 cd /Users/yan/WebstormProjects/jobboard
 
 git add .
-git commit -m "add prisma generate command"
+git commit -m "add prisma generate command 2.0"
 git push
