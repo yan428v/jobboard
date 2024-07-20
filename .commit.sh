95ea3515e49d0595d758a6afc51998bde7b6d63f
yan <yan428@mail.ru>
2024-07-20 15:50:59 +0300

    change commit script

--- .commit.sh
+++ .commit.sh
@@ -2,6 +2,8 @@
 
 cd /Users/yan/WebstormProjects/jobboard
 
+git update-index --assume-unchanged .commit.sh
 git add .
 git commit -m "fix width header bag"
 git push
+git update-index --no-assume-unchanged .commit.sh
